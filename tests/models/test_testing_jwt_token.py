0c6a424b44d0df9d1e24be3383a859d867b651f4
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2021-08-07 12:59:02 -0300

    TESTE - Criado model jwt para encriptografar com sucesso

--- tests/models/test_testing_jwt_token.py
+++ tests/models/test_testing_jwt_token.py
@@ -1,56 +1,59 @@
-from tests.models.test_jwt_token import JWTToken
+from tests.models.test_model_jwt_token import JWTToken
 import pytest
 
 def testClass():
     # Usando valores default de jwt.io, convertidos
-    jWTToken = JWTToken()
-    jWTToken = jWTToken.criarToken(
+    jWTToken = JWTToken(
         payload={"sub": "1234567890","name": "John Doe","iat": 1516239022},
         chave="your-256-bit-secret",
         algoritmo="HS256",
-        header={"typ": "JWT",}
+        header={"typ": "JWT"}
         )
     
-    assert jWTToken == "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.wrJ__8Q_6BcB2ug9370TBuK5JoAjErqsQtYf7aLcFBk"
+    assert jWTToken.payload == {"sub": "1234567890","name": "John Doe","iat": 1516239022}
+    assert jWTToken.chave == "your-256-bit-secret"
+    assert jWTToken.algoritmo == "HS256"
+    assert jWTToken.header == {"typ": "JWT"}
     
+def testClassExceptionFaltaPayload():
+    with pytest.raises(Exception) as ValueError:
+        jWTToken = JWTToken(
+            payload={},
+            chave="your-256-bit-secret",
+            algoritmo="HS256",
+            header={"typ": "JWT"}
+        )
+
+def testClassExceptionFaltaChave():
+    with pytest.raises(Exception) as ValueError:
+        jWTToken = JWTToken(
+            payload={"sub": "1234567890","name": "John Doe","iat": 1516239022},
+            chave="",
+            algoritmo="HS256",
+            header={"typ": "JWT"}
+        )
+        
+def testClassExceptionFaltaAlgoritmo():
+    with pytest.raises(Exception) as ValueError:
+        jWTToken = JWTToken(
+            payload={"sub": "1234567890","name": "John Doe","iat": 1516239022},
+            chave="your-256-bit-secret",
+            algoritmo="",
+            header={"typ": "JWT"}
+        )
+        
 def testClassExceptionFaltaHeader():
     with pytest.raises(Exception) as ValueError:
-        jWTToken = JWTToken()
-        jWTToken = jWTToken.criarToken(
+        jWTToken = JWTToken(
             payload={"sub": "1234567890","name": "John Doe","iat": 1516239022},
             chave="your-256-bit-secret",
             algoritmo="HS256",
             header={}
-            )
+        )
     
-def testClassExceptionFaltaAlgoritmo():
-    with pytest.raises(Exception) as ValueError:
-        jWTToken = JWTToken()
-        jWTToken = jWTToken.criarToken(
-            payload={"sub": "1234567890","name": "John Doe","iat": 1516239022},
-            chave="your-256-bit-secret",
-            algoritmo="",
-            header={"typ": "JWT",}
-            )
 
-def testClassExceptionFaltaChave():
-    with pytest.raises(Exception) as ValueError:
-        jWTToken = JWTToken()
-        jWTToken = jWTToken.criarToken(
-            payload={"sub": "1234567890","name": "John Doe","iat": 1516239022},
-            chave="",
-            algoritmo="HS256",
-            header={"typ": "JWT",}
-            )
+
+
     
-def testClassExceptionFaltaPayload():
-    with pytest.raises(Exception) as ValueError:
-        jWTToken = JWTToken()
-        jWTToken = jWTToken.criarToken(
-            payload={},
-            chave="your-256-bit-secret",
-            algoritmo="HS256",
-            header={"typ": "JWT",}
-            )
-    
+
     
