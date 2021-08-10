d1ebf5cfe5d1879b4aa74c73de41ab6b46e0c7e8
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2021-08-10 10:55:56 -0300

    Controller gerar token criado
    Criado um controller para gerar o token. Implementar depois a questão da chave e testar o controller.

---
+++ src/controladores/c_gerar_token.py
@@ -0,0 +1,24 @@
+from src.models.m_jwt_chave import JWTChave
+from src.models.m_jwt_token import JWTToken
+
+from src.interfaces.i_jwt_encriptografar import IJWTEncriptografar
+
+from src.usecases.uc_criar_chave import UCCriarChave
+from src.usecases.uc_criar_jwt import UCCriarJWT
+
+from fastapi import Response, responses
+
+class CHTTPGerarToken():
+    
+    def gerarToken(self, body: dict, ucCriarJWT : UCCriarJWT):
+        
+        try:
+            modelJWTToken = JWTToken.criarJWTTokenPorDictionary(body)
+            content = ucCriarJWT.criarJWT(modelJWTToken=modelJWTToken, chave="tempkey")
+            response = Response(content=str(content), status_code=200)
+            
+        except:
+            response = Response(content="Error", status_code=400)
+
+        return response
+    
--- src/usecases/uc_servico_gerar_jwt.py
+++ src/usecases/uc_servico_gerar_jwt.py
@@ -1,19 +1,19 @@
 from fastapi import Response, responses
 
-from src.usecases.uc_criar_jwt import UseCaseCriarJWT
-from src.usecases.uc_criar_chave import UseCaseCriarChave
+from src.usecases.uc_criar_jwt import UCCriarJWT
+from src.usecases.uc_criar_chave import UCCriarChave
 
-from src.models.m_jwt_chave import ModelJWTChave
-from src.models.m_jwt_token import ModelJWTToken
+from src.models.m_jwt_chave import JWTChave
+from src.models.m_jwt_token import JWTToken
 
-class ControllerHTTPGerarJWT():
+class UCGerarJWT():
 
     def gerarJWT(self):
 
         try:
             
             #modelChave = ModelJWTChave()
-            modelToken = ModelJWTToken()
+            modelToken = JWTToken()
             
         except:
             response = Exception
