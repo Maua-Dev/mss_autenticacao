725906a40c8babe3102480ca0fdc7e0230669196
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2021-08-19 20:41:12 -0300

    Adicionado controller em main

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Request
 
 from src.controladores.c_gerar_token_fastapi import CGerarTokenFastAPI
+from src.controladores.c_verificar_token import CVerificarToken
 from src.repositorios.jwt.authJWT import AuthJWT
 from src.repositorios.oauth.authOAuth import AuthOAuth
 from src.repositorios.otp import authOTP
@@ -10,6 +11,7 @@ from src.usecases.uc_criar_token import UCCriarToken
 app = FastAPI()
 
 controllerGerarToken = CGerarTokenFastAPI(AuthJWT())
+controllerVerificarToken = CVerificarToken(AuthJWT())
 
 @app.get("/")
 async def root():
@@ -19,3 +21,7 @@ async def root():
 @app.post("/token")
 async def gerarToken(request: Request):
     return controllerGerarToken(await request.json())
+
+@app.post("/verificar")
+async def verificarToken(request: Request):
+    return controllerVerificarToken(await request.body())
