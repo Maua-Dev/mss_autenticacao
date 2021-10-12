9fa43f2d77e96e404f2af4a4ff1d2981ffe2781c
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2021-10-11 21:38:55 -0300

    Criado arquivo python próprio para falha de token

---
+++ src/repositorios/erros/erros_token.py
@@ -0,0 +1,11 @@
+class ErroAssinaturaExpirada(Exception):
+    def __init__(self):
+        super().__init__("O tempo de sua assinatura acabou.")
+        
+class ErroFalhaDecoder(Exception):
+    def __init__(self):
+        super().__init__("O decoder falhou em decodificar.")
+        
+class ErroValidacao(Exception):
+    def __init__(self):
+        super().__init__("A assinatura passada não bate.") #TODO.: ADD A WAY TO ALERT THE SYSTEM IF SOMEONE IS DOING IT?
