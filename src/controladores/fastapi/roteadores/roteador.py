5fa756139649c4436dc4991c725bf4d135163b22
Renan Reschke <renanreschke@hotmail.com>
2021-10-21 19:39:39 -0300

    Rota Atualiza

--- src/controladores/fastapi/roteadores/roteador.py
+++ src/controladores/fastapi/roteadores/roteador.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter
 from .rotas.rotas_mss_info import RotasMssInfo
 from .rotas.rotas_login import RotasLogin
 from .rotas.rotas_cadastrar import RotasCadastrar
+from .rotas.rotas_atualiza import RotasAtualiza
 
 
 
@@ -15,3 +16,4 @@ class Roteador(APIRouter):
         self.include_router(RotasMssInfo())
         self.include_router(RotasLogin(_ctrl))
         self.include_router(RotasCadastrar(_ctrl))
+        self.include_router(RotasAtualiza(_ctrl))
