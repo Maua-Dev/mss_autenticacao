561ef643384777bfb81303988b5a17787abff31d
FelipeGiusti-2000 <61150874+FelipeGiusti-2000@users.noreply.github.com>
2021-10-26 05:16:50 -0300

    Refatoracao das classes de teste de models

--- src/models/chave.py
+++ src/models/chave.py
@@ -1,24 +1,19 @@
 import abc
 from pydantic import BaseModel, validator, ValidationError
 
+
 class Chave(BaseModel):
-    chavePrivada : str
-    senha : str
+    chavePrivada: str
+    senha: str
     
     @validator('chavePrivada', check_fields=False)
     def chavePrivadaNaoVazia(cls, v):
-        if (len(v) == 0):
+        if len(v) == 0:
             raise ValueError("chavePrivada Vazio")
         return v
     
     @validator('senha')
     def senhaNaoVazia(cls, v):
-        if (len(v) == 0):
+        if len(v) == 0:
             raise ValueError("Senha Vazio")
         return v
-        
-    
-        
-
-    
-    
